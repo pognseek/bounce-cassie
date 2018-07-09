add5ad89310c5c3b8b90c852a281e4af96935a22
Katie Poggensee <pognseek@gmail.com>
2018-07-09 14:03:30 -0700

    2D
    Working 2d simulation with instantaneous position control of paddle

--- animatefun.m
+++ animatefun.m
@@ -11,35 +11,45 @@ bball = -sqrt(params.rb^2 - wball.^2);
 dt = 0.01;
 tspan = [t(1):dt:t(end)]';
 if size(x,2) > 2
-    coords = [x(:,1) x(:,3)];
+    coords = [x(:,1) x(:,2) x(:,5) x(:,6) x(:,7)];
 else
     coords = [x(:,1) params.P*ones(size(x(:,1)))];
 end
 intcoords = interp1(t,coords,tspan,'spline');
 
-ax = ceil(max(params.rb,params.PL/2));
-aymax = max(x(:,1) + params.rb*2);
-aymin = params.P - params.rb*2;
-ay = (aymax + aymin) / 2;
+% Break up coordinates
+xb = intcoords(:,1);
+zb = intcoords(:,2);
+xp = intcoords(:,3);
+zp = intcoords(:,4);
+thp= intcoords(:,5);
+
+% ax = ceil(max(params.rb,params.PL/2));
+% aymax = max(x(:,2) + params.rb*2);
+% aymin = params.P - params.rb*2;
+% ay = (aymax + aymin) / 2;
 % a = max(ax,ay);
-a = max(ax,(aymax - aymin)/2);
+% a = max(ax,(aymax - aymin)/2);
+a = 5; ay = 0;
 
 for i=1:length(tspan)
     figure(fignum);
     
     % Ball
-    plot(wball,tball+intcoords(i,1),'b')
+    plot(wball+xb(i),tball+zb(i),'b')
     hold on
-    plot(wball,bball+intcoords(i,1),'b')
-    plot(0,intcoords(i,1),'rx')
+    plot(wball+xb(i),bball+zb(i),'b')
+    plot(xb(i),zb(i),'rx')
     
     % Paddle
-    plot([-params.PL/2 params.PL/2],[intcoords(i,2) intcoords(i,2)],'k')
+    dptemp = params.PL/2 * [cos(thp(i)) sin(thp(i))];
+    plot([xp(i)-dptemp(1),xp(i)+dptemp(1)],[zp(i)-dptemp(2),zp(i)+dptemp(2)],'k')
+%     plot([-params.PL/2 params.PL/2],[intcoords(i,2) intcoords(i,2)],'k')
 %     hold off
     
     % Goal
 %     plot([-a/2 a/2],[params.H params.H],'r--')
-    plot([-a/2 a/2],[params.H params.H],'r--')
+%     plot([-a/2 a/2],[params.H params.H],'r--')
     hold off
     
     
--- sim_bounce1.m
+++ sim_bounce1.m
@@ -7,9 +7,8 @@ mb = 1;             % Mass of the ball (kg)
 mp = 10;            % Mass of the paddle (kg)
 g  = 9.81;          % Gravity (m/s^2)
 params.rb = 0.1;    % Radius of the ball (m)
-params.P  = 0;      % Height of the paddle (m)
+params.P  = 0;      % Position of the paddle (m)
 params.PL = 0.5;    % Length of the paddle (m)
-th_des = pi/2;      % If pi/2, goal: bounce ball vertically in same spot
 
 % Variable
 H0  = 0.5 + rand;   % Initial height (m)
@@ -18,13 +17,7 @@ ke  = rand;         % Coefficient of restitution
 params.H  = abs(randn) + params.rb; % Desired steady-state apex height (m)
 vdes = sqrt(2*g*(params.H - params.rb)); % Desired velocity for H
 
-% 2D Variables
-x0  = 0;            % Initial horizontal position
-dx0 = 5;          % Initial horizontal velocity
-xp0 = 0;            % Initial horizontal position of the paddle
-th0 = 0;            % Initial angle of the paddle
-
-est = false;        % Should we estimate coefficient of restitution?
+est = true;        % Should we estimate coefficient of restitution?
 
 % Simulation parameters
 T  = 3;     % Length of simulation (s)
@@ -33,7 +26,7 @@ printon = true;
 
 %% Initialization
 % States
-x      = [x0; H0; dx0; vb0; xp0; params.P; th0];   % xb, zb, dxb, dzb, xp, zp, thetap
+x      = [H0; vb0]; % Position, velocity of the ball
 xi     = x;         % Initial states (for flight phase)
 t      = 0;         % Time at start of simulation
 t_imp  = 0;         % Time of xi
@@ -54,43 +47,26 @@ while t < T
     % Ball in flight
     if flight
         % Ball dynamics, ballistic motion
-        x(1) = xi(1) + xi(3) * (t - t_imp); % Horizontal position
-        x(2) = xi(2) + xi(4) * (t - t_imp) - g/2 * (t - t_imp)^2;   % Height
-        x(3) = xi(3);   % No horizontal acceleration
-        x(4) = xi(4) - g * (t - t_imp); % Vertical velocity with gravity
-        
-        % Paddle dynamics, track ball
-        x(5) = x(1);    % Horizontally track ball
-        x(6) = x(6);    % Vertically stay put
-        x(7) = x(7);    % Angle stay put
+        x(1) = xi(1) + xi(2) * (t - t_imp) - g/2 * (t - t_imp)^2;
+        x(2) = xi(2) - g * (t - t_imp);
         
         % Impact?
-        if (x(2) <= (x(6) + params.rb)) && (x(4) < 0) % Hits paddle after falling
+        if (x(1) <= (params.P + params.rb)) && (x(2) < 0) % Hits paddle after falling
             flight = false; % Impact event
             t_imp  = t;     % Indicate time of impact
         end
     else    % Impact
         if printon; fprintf([num2str(kest-ke) ' bounce ' num2str(t) '\n']); end
         
-        % Calculations: before impact
-        th_ball_pre = atan(x(4) / x(3));        % Angle of the ball velocity
-        v_ball_pre  = -sqrt(x(3)^2 + x(4)^2);   % Magnitude of ball velocity
-        th_p = (th_des + th_ball_pre) / 2;      % Desired angle of the paddle
-        
-        % Calculations: after impact
-        th_ball_post = - th_ball_pre + 2*th_p;  % Angle of ball velocity, post-impact
-        v_post = -ke * v_ball_pre + (1+ke) * 0; % Magnitude of ball velocity, post-impact
-        
         % Ball dynamics: position
         x(1) = x(1);    % No deformation
-        x(2) = x(2);    % No deformation
-        x(3) = v_post * cos(th_ball_post);
-        x(4) = v_post * sin(th_ball_post);
         
-        % Paddle: no dynamics
-        x(5) = x(5);
-        x(6) = x(6);
-        x(7) = th_p;    % New angle of the paddle
+        % Calculate desired paddle velocity
+        vp = 1/(1+kest) * (kest * x(2) + vdes); vpmat = [vpmat; vp];
+        vpre = x(2);    % Ball velocity pre-impact
+        
+        % Ball dynamics: velocity
+        x(2) = -ke * x(2) + (1+ke) * vp;
         
         % Estimate
         if est
